12a7ab562e89a8016f0bb08fd17528ec2fb79971
sharma-shashank <sharma.shashank990@gmail.com>
2023-07-11 16:15:59 +0530

    add: abort control hook added for api's

--- src/AbortApiController.ts
+++ src/AbortApiController.ts
@@ -1,33 +1,37 @@
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 
-const AbortApiController = (url) => {
+const AbortApiController = (url, page) => {
 
-    const cancelToken = useRef(null);
-    const [data, setData] = useState([]);
-    const [reason, setReason] = useState('');
+	const cancelToken = useRef(null);
+	const [data, setData] = useState([]);
+	const [reason, setReason] = useState('');
+	const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const abortContol = async () => {
-            if (cancelToken.current) {
-            const str = 'cancled due to new request!';
-            cancelToken.current.cancel(str);
-        }
-        cancelToken.current = axios.CancelToken.source();
-        try {
-            let _data = await axios.get(url, {
-                cancelToken: cancelToken.current.token
-            });
-            setData(_data.data);
-            return _data.data;
-        } catch (error){
-            setReason(error.message);
-        }
-    }
-    abortContol();
-    }, [url]);
+	useEffect(() => {
+		const abortContol = async () => {
+			if (cancelToken.current) {
+				const str = 'cancled due to new request!';
+				cancelToken.current.cancel(str);
+			}
+			setLoading(true);
+			cancelToken.current = axios.CancelToken.source();
+			try {
+				let response = await axios.get(url, {
+					cancelToken: cancelToken.current.token
+				});
+				setData(prev => [...prev, ...response.data]);
+				setLoading(false);
+				return response.data;
+			} catch (error) {
+				setReason(error.message);
+				setLoading(false);
+			}
+		}
+		abortContol();
+	}, [page]);
 
-    return { data, reason };
+	return { data, reason, loading };
 }
 
 export default AbortApiController;
