d51c258bb177388d86b208a50b7cdc25967ad2ad
sharma-shashank <sharma.shashank990@gmail.com>
2023-07-11 16:16:37 +0530

    add: crousal component added

---
+++ src/Crousal.tsx
@@ -0,0 +1,26 @@
+import './Crousal.css';
+
+const Crousal = () => {
+	return (
+		<div className="main">
+			<div className="wrapper">
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+				<img className='card-container' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+			</div>
+		</div>
+	);
+}
+
+export default Crousal;
